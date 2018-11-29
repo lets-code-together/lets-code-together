b3c83ea3276b7e06954f881041a4939cc6ce9418
mjpark0113 <44775708+mjpark0113@users.noreply.github.com>
2018-11-29 22:20:25 +0900

    Update 2751.c

--- BaekjoonAlgorithms/09-Sorting/C/2751.c
+++ BaekjoonAlgorithms/09-Sorting/C/2751.c
@@ -15,22 +15,21 @@ static void downheap(int a[], int left, int right)
 		int cr = cl + 1;
 		child = (cr <= right && a[cr] > a[cl]) ? cr : cl;
 
-		if (temp >= a[child])
+		if (temp >= a[child]){
 			break;
-
+		}
 		a[parent] = a[child];
 	}
-
 	a[parent] = temp;
-
 }
 
 void heapsort(int a[], int n)
 {
 	int i;
 
-	for (i = (n - 1) / 2; i >= 0; i--)
+	for (i = (n - 1) / 2; i >= 0; i--){
 		downheap(a, i, n - 1);
+	}
 
 	for (i = n - 1; i > 0; i--) {
 		swap(int, a[0], a[i]);
@@ -50,8 +49,9 @@ int main()
 	}
 	heapsort(x, nx);
 
-	for (i = 0; i < nx; i++)
+	for (i = 0; i < nx; i++){
 		printf("%d\n", x[i]);
+	}
 	free(x);
 
 	return 0;
