eec23cac6362185c4a6c3819259aef9408900137
mjpark0113 <44775708+mjpark0113@users.noreply.github.com>
2018-11-29 22:18:28 +0900

    Update 2750.c

--- BaekjoonAlgorithms/09-Sorting/C/2750.c
+++ BaekjoonAlgorithms/09-Sorting/C/2750.c
@@ -8,9 +8,11 @@ void bubble(int a[], int n)
 {
 	int i, j;
 	for (i = 0; i < n - 1; i++) {
-		for (j = n - 1; j > i; j--)
-			if (a[j - 1] > a[j])
+		for (j = n - 1; j > i; j--){
+			if (a[j - 1] > a[j]){
 				swap(int, a[j - 1], a[j]);
+			}
+		}
 	}
 }
 
@@ -28,9 +30,9 @@ int main()
 
 	bubble(x, nx);
 
-	for (i = 0; i < nx; i++)
+	for (i = 0; i < nx; i++){
 		printf("%d\n", x[i]);
-
+	}
 	free(x);
 
 	return 0;
