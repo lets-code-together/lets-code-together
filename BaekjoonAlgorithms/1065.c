5dbd608af04ad7d8178820047e5fd5dca858c9cd
Hayeon Kim <42827180+hykuuu@users.noreply.github.com>
2018-11-30 18:46:37 +0900

    Update 1065.c

--- BaekjoonAlgorithms/1065.c
+++ BaekjoonAlgorithms/1065.c
@@ -3,30 +3,25 @@
  
 int main(void)
 {
-   int N,cnt;
-   int a, b, c;
-   scanf("%d", &N);
+    int N,cnt;
+    int a, b, c;
+    scanf("%d", &N);
  
-   if (N < 100)
-   {
-      printf("%d", N);
-   }
-   else
-   {
-      cnt = 99;
-      for (int i = 100; i <= N; i++)
-      {
-         a = i / 100;        // 100의 자리
-         b = (i / 10) % 10;  // 10의 자리
-         c = i % 10;         // 1의 자리
+    if (N < 100)
+        printf("%d", N);
+    else
+    {
+        cnt = 99;
+        for (int i = 100; i <= N; i++)
+        {
+            a = i / 100;        // 100의 자리
+            b = (i / 10) % 10;  // 10의 자리
+            c = i % 10;         // 1의 자리
  
-         if ((a - b) == (b - c))
-         {
-                cnt++;
-         }
-      }
+        if ((a - b) == (b - c))
+            cnt++;
+        }
         printf("%d", cnt);
     }
     return 0;
 }
-
