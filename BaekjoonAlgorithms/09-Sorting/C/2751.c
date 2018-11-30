a3fc2abe755c2587bac9088412da6808513be3f5
mjpark0113 <44775708+mjpark0113@users.noreply.github.com>
2018-11-30 18:18:52 +0900

    Update 2751.c

--- BaekjoonAlgorithms/09-Sorting/C/2751.c
+++ BaekjoonAlgorithms/09-Sorting/C/2751.c
@@ -10,14 +10,15 @@ static void downheap(int a[], int left, int right)
 	int child;
 	int parent;
 
-	for (parent = left; parent < (right + 1) / 2; parent = child) {
+	for (parent = left; parent < (right + 1) / 2; parent = child)
+	{
 		int cl = parent * 2 + 1;
 		int cr = cl + 1;
 		child = (cr <= right && a[cr] > a[cl]) ? cr : cl;
 
-		if (temp >= a[child]){
+		if (temp >= a[child])
 			break;
-		}
+		
 		a[parent] = a[child];
 	}
 	a[parent] = temp;
@@ -27,11 +28,13 @@ void heapsort(int a[], int n)
 {
 	int i;
 
-	for (i = (n - 1) / 2; i >= 0; i--){
+	for (i = (n - 1) / 2; i >= 0; i--)
+	{
 		downheap(a, i, n - 1);
 	}
 
-	for (i = n - 1; i > 0; i--) {
+	for (i = n - 1; i > 0; i--)
+	{
 		swap(int, a[0], a[i]);
 		downheap(a, 0, i - 1);
 	}
@@ -44,12 +47,14 @@ int main()
 	scanf_s("%d", &nx);
 	x = calloc(nx, sizeof(int));
 
-	for (i = 0; i < nx; i++) {
+	for (i = 0; i < nx; i++)
+	{
 		scanf_s("%d", &x[i]);
 	}
 	heapsort(x, nx);
 
-	for (i = 0; i < nx; i++){
+	for (i = 0; i < nx; i++)
+	{
 		printf("%d\n", x[i]);
 	}
 	free(x);
