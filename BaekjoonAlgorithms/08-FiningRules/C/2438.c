b8c0dc3c348217b5b07dde22e15868f9b34efb59
Yeongjo Cho <dudjjjo@gmail.com>
2019-01-02 22:19:26 +0900

    8steps

---
+++ BaekjoonAlgorithms/08-FiningRules/C/2438.c
@@ -0,0 +1,15 @@
+/*첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제*/
+#include <stdio.h>
+main()
+{
+	int n, i, j;
+
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= i; j++)
+			printf("*");
+		printf("\n");
+	}
+}
