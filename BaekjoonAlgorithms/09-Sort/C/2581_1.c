1d9fa59bd8145ddfc84bddcf05119eae7a494124
Yeongjo Cho <dudjjjo@gmail.com>
2019-01-04 20:00:17 +0900

    2581_1.c

---
+++ BaekjoonAlgorithms/09-Sort/C/2581_1.c
@@ -0,0 +1,27 @@
+/*자연수 M,N이 주어질때 M이상 N이하의 소수의 합과 가장작은 소수를 구하라*/
+#include<stdio.h>
+int main()
+{
+	int M, N, cnt = 0, sum = 0, min = 10001;
+	scanf("%d\n%d", &M, &N);
+
+	for (int i = M; i <= N; i++)
+	{
+		for (int j = 2; j < i; j++)
+		{
+			if (i%j == 0)
+				cnt++;
+		}
+		if (cnt == 0 && i != 1)
+		{
+			sum += i;
+			if (min > i)
+				min = i;
+		}
+		cnt = 0;
+	}
+	if (min == 10001)
+		printf("-1\n");
+	else
+		printf("%d\n%d\n", sum, min);
+}
