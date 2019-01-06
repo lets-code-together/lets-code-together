cfcf5a14974d385f8c8d491224d8b156a65f36f9
Yeongjo Cho <dudjjjo@gmail.com>
2019-01-06 23:06:53 +0900

    1929_1.c

---
+++ BaekjoonAlgorithms/10-DecimalNumber/C/1929_1.c
@@ -0,0 +1,16 @@
+/*M이상 N이하의 소수를 모두 출력해라(기존의 for이중구문X,에라스토테네스O */
+#include<stdio.h>
+main()
+{
+	int M, N, i, j, a[1000001] = { 0, };
+	a[1] = 1;
+	scanf("%d %d", &M, &N);
+
+	for (i = 2; i <= N; i++)
+	{
+		for (j = 2; i*j <= N; j++)
+			a[j*i] = 1;
+	}
+	for (i = M; i <= N; i++)
+		if (!a[i]) printf("%d\n", i);
+}
