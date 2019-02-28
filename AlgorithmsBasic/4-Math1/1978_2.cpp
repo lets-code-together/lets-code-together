ef64ee19457ef2cbaafcfd9d70aaeb004ba9b4d7
YeongjoCho <44775943+YeongjoCho@users.noreply.github.com>
2019-02-28 19:35:06 +0900

    Update 1978_2.cpp

--- AlgorithmsBasic/4-Math1/1978_2.cpp
+++ AlgorithmsBasic/4-Math1/1978_2.cpp
@@ -1,6 +1,6 @@
 /*
-�־��� �� N�� �߿��� �Ҽ��� �� ������ ã�Ƽ� ����ϴ� ���α׷��� �ۼ��Ͻÿ�.
-(�����佺�׳׽���ü �˻�)(���ڰ� Ŀ����� �Ҽ�ã���� �����佺�׳׽���ü �̿�)
+주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력하는 프로그램을 작성하시오.
+(에라토스테네스의체 검색)(숫자가 커질경우 소수찾을때 에라토스테네스의체 이용)
 */
 
 #include <iostream>
@@ -9,17 +9,19 @@
 using namespace std;
 const int MAX = 100;
 
-int minFactor[1001]; //i�� ���� ���� ���μ�(i�� �Ҽ��� ��� �ڱ� �ڽ�)
+int minFactor[1001]; //i의 가장 작은 소인수(i가 소수인 경우 자기 자신)
 
 void eratosthenes(void)
 
 {
 	minFactor[0] = minFactor[1] = -1;
+	
 	for (int i = 2; i <= 1001; i++)
 	{
 		minFactor[i] = i;
 	}
 	int sqrtN = int(sqrt(1001));
+	
 	for (int i = 2; i <= sqrtN; i++)
 	{
 		if (minFactor[i] == i)
@@ -27,7 +29,9 @@ void eratosthenes(void)
 			for (int j = i * i; j <= 1001; j += i)
 			{
 				if (minFactor[j] == j)
+				{	
 					minFactor[j] = i;
+				}
 			}
 		}
 
