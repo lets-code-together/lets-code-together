43e39b73f6bb7352abac1436a372fd2cd5172f0e
YeongjoCho <44775943+YeongjoCho@users.noreply.github.com>
2019-02-28 19:28:39 +0900

    Update 2743_2.cpp

--- AlgorithmsBasic/2-DataStructure1/2743_2.cpp
+++ AlgorithmsBasic/2-DataStructure1/2743_2.cpp
@@ -1,8 +1,9 @@
-/*���ĺ����θ� �̷���� �ܾ �Է¹޾�, �� ���̸� ����ϴ� ���α׷��� �ۼ��Ͻÿ�.*/
+/*알파벳으로만 이루어진 단어를 입력받아, 그 길이를 출력하는 프로그램을 작성하시오.*/
 #include <iostream>
 #include <string>
 using namespace std;
-int main() {
+int main()
+{
 	string x;
 	cin >> x;
 	cout << x.size() << '\n';
