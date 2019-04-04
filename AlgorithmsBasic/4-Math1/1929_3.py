d891a66e3c3b3ddc78c7fb8328dd2525c7d99f71
Ahyun Jeong <ahjung2009@naver.com>
2019-04-04 19:34:33 +0900

    190404

--- AlgorithmsBasic/4-Math1/1929_3.py
+++ AlgorithmsBasic/4-Math1/1929_3.py
@@ -1,17 +1,31 @@
 # 소수 구하기
 # M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.
 
-M, N = int(input()).split()
-
 
 def is_prime(X):
     if X < 2:
         return False
-    elif X >= 2 or X <= 3:
+    if X in (2, 3):
         return True
-    elif X % 2 == 0 or X % 3 == 0:
+    if X % 2 == 0 or X % 3 == 0:
         return False
-    elif X < 9:
+    if X < 9:
         return True
 
-    
+    k = 5
+    l = X ** 0.5
+
+    while k <= l:
+        if X % k == 0 or X % (k+2) == 0:
+            return False
+        k += 6
+
+    return True
+
+
+M, N = map(int, input().split())
+
+for i in range(N - M + 1):
+    i += M
+    if is_prime(i) == True:
+        print(i)
---
+++ AlgorithmsBasic/4-Math1/6588_3.py
@@ -0,0 +1,46 @@
+# 골드바흐의 추측
+# 4보다 큰 모든 짝수는 두 홀수 소수의 합으로 나타낼 수 있다.
+# 백만 이하의 모든 짝수에 대해서, 이 추측을 검증하는 프로그램을 작성하시오.
+def is_prime(X):
+    if X < 2:
+        return False
+    if X in (2, 3):
+        return True
+    if X % 2 == 0 or X % 3 == 0:
+        return False
+    if X < 9:
+        return True
+
+    k = 5
+    l = X ** 0.5
+
+    while k <= l:
+        if X % k == 0 or X % (k+2) == 0:
+            return False
+        k += 6
+
+    return True
+
+n = int(input())  # 짝수 정수
+# n 보다 작은 소수의 리스트 만들기
+prime = []
+
+for i in range(n):
+    if is_prime(i) == True:
+        prime.append(i)
+
+# 소수의 합으로 n을 나타내기
+i = 0
+while i < int(len(prime)/2 + 1):
+    for j in range(int(len(prime))):
+        j = int(len(prime)/2) - j
+        if prime[int(i)] + prime[int(j)] == n:
+            print("%d = %d + %d" %(n, prime[int(i)], prime[int(j)]))
+            break
+
+            if prime[int(i)] + prime[int(j)] == n:
+                break
+            else:
+                i += 1
+if prime[int(i)] + prime[int(j)] != n:
+    print("Goldbach's conjecture is wrong.")
