fe24b30ce5d2fbbec3c30574cf264c3b526e2be1
Ahyun Jeong <ahjung2009@naver.com>
2019-04-06 15:43:39 +0900

    190406

--- AlgorithmsBasic/4-Math1/6588_3.py
+++ AlgorithmsBasic/4-Math1/6588_3.py
@@ -1,6 +1,7 @@
 # 골드바흐의 추측
 # 4보다 큰 모든 짝수는 두 홀수 소수의 합으로 나타낼 수 있다.
 # 백만 이하의 모든 짝수에 대해서, 이 추측을 검증하는 프로그램을 작성하시오.
+# 시간초과
 def is_prime(X):
     if X < 2:
         return False
@@ -21,26 +22,30 @@ def is_prime(X):
 
     return True
 
-n = int(input())  # 짝수 정수
-# n 보다 작은 소수의 리스트 만들기
-prime = []
-
-for i in range(n):
-    if is_prime(i) == True:
-        prime.append(i)
 
 # 소수의 합으로 n을 나타내기
-i = 0
-while i < int(len(prime)/2 + 1):
-    for j in range(int(len(prime))):
-        j = int(len(prime)/2) - j
-        if prime[int(i)] + prime[int(j)] == n:
-            print("%d = %d + %d" %(n, prime[int(i)], prime[int(j)]))
-            break
+while True:
+    n = int(input())  # 짝수 정수
+    if n == 0:
+        break
+    # 백만보다 작은 소수의 리스트 만들기
+    prime = []
 
+    for i in range(1000000):
+        if is_prime(i) == True:
+            prime.append(i)
+    i = 0
+    while i < int(len(prime)/2 + 1):
+        for j in range(int(len(prime))):
+            j = int(len(prime)/2) - j
             if prime[int(i)] + prime[int(j)] == n:
+                print("%d = %d + %d" %(n, prime[int(i)], prime[int(j)]))
                 break
-            else:
-                i += 1
-if prime[int(i)] + prime[int(j)] != n:
-    print("Goldbach's conjecture is wrong.")
+
+        if prime[int(i)] + prime[int(j)] == n:
+            break
+        else:
+            i += 1
+
+    if prime[int(i)] + prime[int(j)] != n:
+        print("Goldbach's conjecture is wrong.")
